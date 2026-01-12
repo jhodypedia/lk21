eb64be17e293bff06140713da00b6cd86bd8b108
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2026-01-12 19:30:00 +0700

    Update constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Domain LK21 sering berubah, ganti di sini jika mati
-    BASE_URL: 'https://tv7.lk21official.cc', 
+    BASE_URL: 'https://lk21.de', 
     
     HEADERS: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
